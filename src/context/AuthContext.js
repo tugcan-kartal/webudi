fc73b2e6a09e065852d9b58bb112b5513a9575f9
tugcan-kartal <tugcankartal01@gmail.com>
2023-06-02 16:57:00 +0300

    updated

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,23 +6,34 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
 
-    const register=(name,email,password)=>{
-       axios.post(`${BASE_URL}/register`,{
-        name,
-        email,
-        password,
-       })
-       .then(res=>{
-        let userInfo=res.data;
-        console.log(userInfo);
-       })
-       .catch(e=>{
-        console.log(`register error ${e}`);
-       });
-    };
+    const register = (name, email, password) => {
+        axios
+            .post(`${BASE_URL}/register`, {
+                name,
+                email,
+                password,
+                password_confirmation: password,
+            }, {
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            })
+            .then((res) => {
+                let userInfo = res.data;
+                console.log(userInfo);
+            })
+            .catch((error) => {
+                if (error.response) {
+                  console.log(error.response.data); // Hata yanıtını logla
+                } else {
+                  console.log(`register error ${error}`);
+                }
+              });
+      };
+      
 
     return (
-        <AuthContext.Provider value={register}>
+        <AuthContext.Provider value={{register}}>
             {children}
         </AuthContext.Provider>
     )
