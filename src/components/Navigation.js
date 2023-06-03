5a264cb24b369037099d8265bd4ae73d51526b03
tugcan-kartal <tugcankartal01@gmail.com>
2023-06-03 14:31:51 +0300

    updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import { AuthContext } from '../context/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
 
 const Navigation = () => {
 
+  const {userInfo} = useContext(AuthContext);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Register" component={RegisterScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {userInfo.token 
+        ? 
+          (<Stack.Screen name="Home" component={HomeScreen} />) 
+        : 
+          (<> 
+            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Register" component={RegisterScreen} options={{headerShown: false}}/> 
+          </>)
+        }
+        
+        
       </Stack.Navigator>
     </NavigationContainer>
   )
