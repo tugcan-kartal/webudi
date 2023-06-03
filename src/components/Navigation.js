e04ab5c9bf3540ab52c46f9ecaeed6d41eac4c7a
tugcan-kartal <tugcankartal01@gmail.com>
2023-06-03 16:28:28 +0300

    up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,9 +15,17 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        {userInfo.token && userInfo.message === 'Giriş Başarılı' ? (
           <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+        ) : (
+          <>
+            {userInfo.message === 'Kayıt Başarılı' ? (
+              <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            ) : (
+              <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+            )}
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
