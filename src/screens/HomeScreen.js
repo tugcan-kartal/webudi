bdae237d670b263472f7c6d94e604dafef60a552
tugcan-kartal <tugcankartal01@gmail.com>
2023-06-04 16:29:16 +0300

    updatedd

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,20 +11,25 @@ const HomeScreen = () => {
   }, []);
 
   const renderItem = ({ item }) => (
-    <View key={item.id}>
-      <Text>{item.title}</Text>
-      <Text>{item.description}</Text>
-      <Image source={{ uri: item.image }} style={{ width: 200, height: 200 }} />
+    <View className="bg-white p-6 border-[1px] border-gray-400 rounded-lg shadow-2xl my-[10%]" key={item.id}>
+      <Image source={{ uri: item.image }} className="w-[75vw] h-[25vh]" />
+      <Text className="text-gray-500 text-xl mt-[1vh]">{item.category.title}</Text>
+      <Text className="text-lg font-semibold">{item.title}</Text>
+      <Text className="text-md font-bold text-blue-500">{item.price} $</Text>
     </View>
   );
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex items-center justify-center bg-gray-50 pb-[15vh]">
       <Spinner visible={isLoading} />
       {userInfo && (
-        <Text className="text-lg mb-8">Welcome {userInfo.user.name}</Text>
+        <View className="flex-row gap-x-4 pt-[5vh] pb-[5vh]">
+          <Text className="text-xl">Welcome</Text>
+          <Text className="text-xl font-bold">{userInfo.user.name}</Text>
+        </View>
       )}
       <FlatList
+        className="mb-[5vh]"
         data={products}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
